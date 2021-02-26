e6d05dc4e1549d30f06c76c53cabeb9b0901402e
EMILS <emils.bisenieks93@gmail.com>
2021-02-26 15:23:38 +0200

    font update

--- gatsby/src/styles/Typography.js
+++ gatsby/src/styles/Typography.js
@@ -1,17 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 const Typography = createGlobalStyle`
-    html {
+    * {
         font-family: Nunito, sans-serif, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue';
     }
 
     p, li, span {
         letter-spacing: .03em;
         line-height: 1.6em;
+        /* font-family: Nunito, sans-serif; */
     }
 
     h1,h2,h3,h4,h5,h6 {
-        font-family: Nunito, sans-serif;
+        /* font-family: Nunito, sans-serif; */
         font-weight: normal;
         margin: 0;
     }
